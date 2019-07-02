7423d08bc0ffbeff7a307b77403aa11b8ff4a21b
Li Jiacheng <839492148@qq.com>
2019-07-03 00:46:51 +0800

    completed

--- 爬虫入门练习-小说爬虫/all_text.py
+++ 爬虫入门练习-小说爬虫/all_text.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+# 定义类
 class download(object):
   # 定义属性
   def __init__(self):
@@ -16,8 +17,8 @@ class download(object):
     div = BeautifulSoup(req.text).find_all('div', class_ = 'listmain')
     # 查找集合中的a标签
     titles = BeautifulSoup(str(div[0])).find_all('a')
-    self.nums = len(titles[4:-3])
-    for each in titles[4:10]:
+    self.nums = len(titles[4:6])
+    for each in titles[4:6]:
       self.title.append(each.string)
       self.urls.append(self.sever + each.get('href'))
   
@@ -27,3 +28,22 @@ class download(object):
     content = BeautifulSoup(req.text)
     texts = content.fond_all('div', class_ = 'showtxt')
     texts = texts[0].text.replace('\xa0'*8, '\n\n')
+    return texts
+  
+  # 将爬取的文章写入文件
+  def writer(self, name, path, text):
+  	write_flag = True
+  	# 文件写入函数？？
+  	with open(path, 'a', encoding = 'utf-8') as f:
+  		f.write(name + '\n')
+  		f.writelines(text)
+  		f.write('\n\n\n')
+
+
+# 调用函数
+if __name__ == '__main__':
+	# 参数？？
+  dl = download()
+  dl.get_url()
+  for i in range(dl.nums):
+    dl.writer(dl.title[i], '一念永恒.txt', dl.get_content(dl.urls[i]))
